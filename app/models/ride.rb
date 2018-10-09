ac9af3dacf204fb1bdfd42c06c72cd1453026a0f
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-10-09 15:45:31 +0000

    automatically backed up by learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,14 +2,6 @@ class Ride < ActiveRecord::Base
   belongs_to :attraction
   belongs_to :user
 
-  def enough_tickets?
-    self.attraction.tickets < user.tickets
-  end
-
-  def tall_enough?
-    self.attraction.min_height <= user.height
-  end
-
   def take_ride
     if enough_tickets? && tall_enough?
       self.user.tickets -= self.attraction.tickets
@@ -21,7 +13,19 @@ class Ride < ActiveRecord::Base
     elsif enough_tickets? && !tall_enough?
       "Sorry. You are not tall enough to ride the #{self.attraction.name}."
     else
-      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
+      . You are not tall enough to ride the #{self.attraction.name}."
     end
+  end
+  
+  def enough_tickets?
+    self.attraction.tickets < user.tickets
+  end
+
+  def tall_enough?
+    self.attraction.min_height <= user.height
+  end
+
+  def not_enough_tickets
+    "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
   end
 end
