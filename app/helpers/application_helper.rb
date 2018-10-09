ba6495db6c9fffde047d55d52ead229d8b123c9f
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-10-09 19:06:38 +0000

    automatically backed up by learn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def display_errors(object)
+    if object.errors.any?
+      object.errors.full_messages.each do |message|
+        <li><%= message %></li>
+      end
+    end
+  end
 end
